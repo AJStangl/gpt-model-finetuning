ca8149eb48f1c62d9324a586fe3e8147e7a3c726
AJStangl <ajstangl@gmail.com>
2022-08-28 10:34:34 -0400

    this is cool

--- src/cli_wrapper.py
+++ src/cli_wrapper.py
@@ -3,6 +3,7 @@ from logging import getLogger
 
 import asyncclick as click
 
+from src.text_generation.model_text_generator import ModelTextGenerator
 from src.training_module.model_fine_tuning import ModelFineTuner
 from src.training_module.training_finetuning_collector import FineTuningDataCollector
 
@@ -15,7 +16,9 @@ def cli1():
 @click.group()
 def cli2():
 	pass
-
+@click.group()
+def cli3():
+	pass
 
 @cli1.command()
 @click.option("--sub", prompt='specify a single subreddit name')
@@ -23,7 +26,6 @@ async def get_fine_tuning(sub: str):
 	"""Command for downloading finetuning data"""
 	collector: FineTuningDataCollector = FineTuningDataCollector()
 	await collector.get_top_100(subreddit=sub)
-
 
 @cli2.command()
 @click.option("--bot-label", prompt='specify a bot label for bot', default='generic')
@@ -40,8 +42,13 @@ async def train_model(bot_label: str, data: str):
 
 	model_fine_tuner.train_model(model, train_file=train_file, eval_file=eval_file)
 
+@cli3.command()
+async def gen_text():
+	model_path: str = "D:\\code\\repos\\quick_training\\only_fans_bot_2\\best_model"
+	text_generator: ModelTextGenerator = ModelTextGenerator()
+	text_generator.generate_text_with_no_wrapper(model_path, "<|sols r/ExtremeCombat|><|sot|>How are you doing today?<|eot|><|sol|>It feels nice out right?<|eol|><|sor|>")
 
-cli = click.CommandCollection(sources=[cli1, cli2])
+cli = click.CommandCollection(sources=[cli1, cli2, cli3])
 
 if __name__ == '__main__':
 	logging.basicConfig(level=logging.INFO)
---
+++ src/text_generation/model_text_generator.py
@@ -0,0 +1,76 @@
+import torch
+import time
+from logging import getLogger
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+logger = getLogger("ModelTextGenerator")
+
+class ModelTextGenerator:
+	def __init__(self):
+		self.text_generation_parameters = {
+			'max_length': 1024,
+			'num_return_sequences': 1,
+			'prompt': None,
+			'temperature': 0.8,
+			'top_k': 40,
+			'top_p': .8,
+			'do_sample': True,
+			'repetition_penalty': 1.08,
+			'stop_token': '<|endoftext|>'
+		}
+
+	def generate_text_with_no_wrapper(self, model_path: str, prompt_text: str, cuda_device: int = 1) -> str:
+		start_time = time.time()
+		try:
+			device = torch.device(f"cuda:{cuda_device}" if torch.cuda.is_available() else "cpu")
+
+			tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+
+			encoded_prompt = tokenizer.encode(prompt_text, add_special_tokens=False, return_tensors="pt")
+
+			if len(encoded_prompt.data[0]) > self.text_generation_parameters['max_length']:
+				logger.info(
+					f":: Size of Tensor {encoded_prompt.data[0]} > {self.text_generation_parameters['max_length']}. Rejecting Attempt to Process Input")
+				return None
+
+			generation_prompt = tokenizer([prompt_text], add_special_tokens=False, return_tensors="pt")
+
+			generation_prompt = generation_prompt.to(device)
+
+			model = GPT2LMHeadModel.from_pretrained(model_path)
+
+			model = model.to(device)
+			output_sequences = model.generate(
+				inputs=generation_prompt['input_ids'],
+				max_length=1024,
+				min_length=100,
+				do_sample=True,
+				top_k=40,
+				temperature=0.8,
+				repetition_penalty=1.08,
+				attention_mask=generation_prompt['attention_mask'],
+				stop_token='<|endoftext|>',
+			)
+			text_generations = []
+
+			for i in range(self.text_generation_parameters['num_return_sequences']):
+				decoded_text = tokenizer.decode(output_sequences[i], skip_special_tokens=False)
+				if decoded_text in ['[removed]'] or decoded_text == "":
+					raise Exception("Text No Good Try Again!")
+				text_generations.append(decoded_text)
+				decoded_text.replace(prompt_text, "")
+				print(f"Generated {i}: {decoded_text}")
+
+			end_time = time.time()
+			duration = round(end_time - start_time, 1)
+
+			print(f'{len(text_generations)} sample(s) of text generated in {duration} seconds.')
+
+			return max(text_generations, key=len)
+
+		except Exception as e:
+			logger.error(f":: An error has occurred while attempting to generate text")
+			logger.error(e)
+			raise e
+		finally:
+			pass
